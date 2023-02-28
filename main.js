6c55747ac32bd26925e3567f868897fbdbd66a86
JoseReyesY <92343754+JoseReyesY@users.noreply.github.com>
2023-02-28 12:02:29 -0600

    Se modifico el codigo para que funcione mediante un arreglo de inputs

--- main.js
+++ main.js
@@ -4,27 +4,31 @@ const emailInput = document.getElementById ( 'email' );
 const phoneNumberInput = document.getElementById ( 'phone-number' );
 const labelContainer = document.querySelectorAll ( '.label--container' );
 
+let inputList = [];
 
-nameInput.addEventListener( 'blur', function ( e ) {
-    if ( e.target.value === '' ) {
-        // Permite insertar elementos HTML de manera dinámicamente. El primer argumento es para indicar en donde se pondrá el elemento a añadir
-        labelContainer[0].insertAdjacentHTML( 'beforeEnd', `<span class="warning">This field is required</span>` );
-        nameInput.style.outline = '1px solid hsl(354, 84%, 57%)';
-    } else {
-        
-    }
-});
+inputList.push( nameInput, emailInput, phoneNumberInput );
 
-emailInput.addEventListener( 'blur', function ( e ) {
-    if ( e.target.value === '' ) {
-        labelContainer[1].insertAdjacentHTML( 'beforeEnd', `<span class="warning">This field is required</span>` );
-        emailInput.style.outline = '1px solid hsl(354, 84%, 57%)';
-    }
-} );
+function checkInput () {
+    // nameInput.addEventListener( 'blur', function ( e ) {
+    //     if ( e.target.value === '' ) {
+    //         // Permite insertar elementos HTML de manera dinámicamente. El primer argumento es para indicar en donde se pondrá el elemento a añadir
+    //         labelContainer[0].insertAdjacentHTML( 'beforeEnd', `<span class="warning">This field is required</span>` );
+    //         nameInput.style.outline = '1px solid hsl(354, 84%, 57%)';
+    //     }
+    // });
 
-phoneNumberInput.addEventListener( 'blur', function ( e ) {
-    if ( e.target.value === '' ) {
-        labelContainer[2].insertAdjacentHTML( 'beforeEnd', `<span class="warning">This field is required</span>` );
-        phoneNumberInput.style.outline = '1px solid hsl(354, 84%, 57%)';
-    }
-} );
+    inputList.forEach( ( input ) => {
+        input.addEventListener ( 'blur', function ( e ) {
+            if ( e.target.value === '' ) {
+                // Permite insertar elementos HTML de manera dinámicamente. El primer argumento es para indicar en donde se pondrá el elemento a añadir
+                labelContainer[ inputList.indexOf( input ) ].insertAdjacentHTML( 'beforeEnd', `<span class="warning">This field is required</span>` );
+                input.style.outline = '1px solid hsl(354, 84%, 57%)';
+            } else {
+                input.style.outline = '1px solid hsl(229, 24%, 87%)';
+            }
+        } )
+    });
+}
+
+checkInput();
+
