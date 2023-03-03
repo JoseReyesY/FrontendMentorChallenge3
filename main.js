d58321d74ea0aa5d919b54ca4f98b6be4493581a
JoseReyesY <92343754+JoseReyesY@users.noreply.github.com>
2023-03-02 20:24:37 -0600

    Se agrego el paso 2

--- main.js
+++ main.js
@@ -3,10 +3,18 @@ const nameInput = document.getElementById ( 'name' );
 const emailInput = document.getElementById ( 'email' );
 const phoneNumberInput = document.getElementById ( 'phone-number' );
 const labelContainer = document.querySelectorAll ( '.label--container' );
+const sentButton = document.querySelector( '.next-step-button' );
+const goBackButton = document.querySelector( '.back-step-button' );
+
+const stepContainer1 = document.getElementById( 'step1-total--container' );
+const stepContainer2 = document.getElementById( 'step2-total--container' );
 
 let inputList = [];
 
 inputList.push( nameInput, emailInput, phoneNumberInput );
+
+sentButton.addEventListener( 'click', checkInputButton);
+goBackButton.addEventListener( 'click', goBack );
 
 function checkInput () {
     // nameInput.addEventListener( 'blur', function ( e ) {
@@ -21,14 +29,34 @@ function checkInput () {
         input.addEventListener ( 'blur', function ( e ) {
             if ( e.target.value === '' ) {
                 // Permite insertar elementos HTML de manera dinámicamente. El primer argumento es para indicar en donde se pondrá el elemento a añadir
-                labelContainer[ inputList.indexOf( input ) ].insertAdjacentHTML( 'beforeEnd', `<span class="warning">This field is required</span>` );
+                labelContainer[ inputList.indexOf( input ) ].lastElementChild.innerText = 'This field is required';
                 input.style.outline = '1px solid hsl(354, 84%, 57%)';
             } else {
+                labelContainer[ inputList.indexOf( input ) ].lastElementChild.innerText = '';
                 input.style.outline = '1px solid hsl(229, 24%, 87%)';
             }
-        } )
+        } );
     });
+
+
+}
+
+function checkInputButton () {
+    inputList.forEach( ( input ) => {
+        if ( input.value.length === 0 ) {
+            // Permite insertar elementos HTML de manera dinámicamente. El primer argumento es para indicar en donde se pondrá el elemento a añadir
+            labelContainer[ inputList.indexOf( input ) ].lastElementChild.innerText = 'This field is required';
+            input.style.outline = '1px solid hsl(354, 84%, 57%)';
+        }
+    } );
+}
+
+function goBack () {
+    stepContainer1.style.display = 'block';
+    stepContainer2.style.display = 'none';
 }
 
 checkInput();
+
+
 
